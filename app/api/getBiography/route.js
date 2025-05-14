9238b042274d2373c6a0905ada7c3629de61a9a9
ra2.dndlab@gmail.com <ra2.dndlab@gmail.com>
2025-05-14 18:01:04 +0600

    Fix: update image filenames to match references

--- app/api/getBiography/route.js
+++ app/api/getBiography/route.js
@@ -11,19 +11,19 @@ export async function GET() {
     {
       title: "Art Institute and Formative Years",
       yearRange: "1950–1954",
-      imageUrl: "/images/artInstitute.png", // optional
+      imageUrl: "/images/ArtInstitute.png", // optional
       content: "He joined the Government Art Institute in Dhaka after Matriculation. Under the guidance of Zainul Abedin, Qayyum developed his skills in fine arts and graphic art, graduating in 1954.",
     },
     {
       title: "Career and Contributions",
       yearRange: "1955–1984",
-      imageUrl: "/images/career&contribution.png",
+      imageUrl: "/images/career&Contribution.png",
       content: "Qayyum Chowdhury became a teacher at the same institute he graduated from. He played a vital role in art education in East Pakistan and Bangladesh. He was deeply involved in curriculum design and nurturing young talent.",
     },
     {
       title: "Recognition and Legacy",
       yearRange: "1985–2014",
-      imageUrl: "/images/recog&legacy.png",
+      imageUrl: "/images/recog&Legacy.png",
       content: "He was awarded the Ekushey Padak in 1984. His contributions include iconic book covers, murals, and illustrations. He passed away in 2014 while delivering a speech at an event.",
     }
   ];
